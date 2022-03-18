0f1f4377f67dc01a0da223eb6583ca532ebb6f2f
Marlena Tsang <Marlena345@gmail.com>
2022-03-18 14:34:37 -0400

    added index, create methods

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,17 @@
 class RestaurantsController < ApplicationController
+
+  def index
+    render json: Restaurant.all
+  end
+
+  def create
+    restaurant = Restaurant.create!(restaurant_params)
+    render json: restaurant
+  end
+
+  private
+  def restaurant_params
+    params.permit(:name, :food_type, :description)
+  end
+
 end
