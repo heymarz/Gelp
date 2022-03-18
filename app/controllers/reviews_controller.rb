464d13227de960bf4efba128be16d3dd495dea6b
Marlena Tsang <Marlena345@gmail.com>
2022-03-18 14:34:54 -0400

    added index, create, show methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,20 @@
 class ReviewsController < ApplicationController
-  
+  def index
+    render json: Reviews.all
+  end
+
+  def show
+    restaurant = Review.find_by(id: review_params[:id])
+    render json: restaurant, include: [:restaurants, :user]
+  end
+
+  def create
+    restaurant_review = Restaurant.review.create!(review_params)
+    render json: restaurant_review, status: :created
+  end
+
+  private
+  def review_params
+    params.permit(:user_id, :restaurant_id, :review_description, :rating)
+  end
 end
