8a360b9dddeff821ddb51c1a944b4b4d82c9f798
Marlena Tsang <Marlena345@gmail.com>
2022-03-18 10:59:28 -0400

    Edited/add attributions

--- db/migrate/20220317202920_create_reviews.rb
+++ db/migrate/20220317202920_create_reviews.rb
@@ -2,6 +2,7 @@ class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
       t.integer :user_id
+      t.integer :restaurant_id
       t.text :review_description
       t.integer :rating
 
