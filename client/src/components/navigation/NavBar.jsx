d435797bf1391a972d29f532c07d4ec5f21b2259
Marlena Tsang <Marlena345@gmail.com>
2022-03-19 23:17:01 -0400

    added toggle for login/out links

--- client/src/components/navigation/NavBar.jsx
+++ client/src/components/navigation/NavBar.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-function NavBar({ user, setUser }) {
+function NavBar({loggedIn, logoutUser}) {
+  function loggedOutLinks(){
+    return (
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/signup">Sign up</Link></li>
+        <li><Link to="/login">Log in</Link></li>
+      </ul>
+    )
+  }
+  function handleLogout(e){
+    e.preventDefault();
+    logoutUser();
+  }
+
+  function loggedInLinks(){
+    return(
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><a href="#!" onClick={handleLogout}>Log out</a></li>
+      </ul>
+    )
+  }
   return (
     <div>
-      <Link to="/">Login</Link>
-      <Link to="/new">Add new Restaurant</Link>
-      
+      {loggedIn ? loggedInLinks() : loggedOutLinks()}
     </div>
   )
 }
