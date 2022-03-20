72ea534727a67e26bc5c934a8409031604f1a4c8
Marlena Tsang <Marlena345@gmail.com>
2022-03-19 23:18:15 -0400

    added login home component

--- client/src/components/pages/Login.jsx
+++ client/src/components/pages/Login.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react';
+import SignupForm from '../SignupForm';
+import LoginForm from '../LoginForm';
 
-function Login (){
+function Login ({loginUser, addErrors, clearErrors}){
+  const [showLogin, setShowLogin] = useState(true)
+
   return (
-    <h1>This is the log in</h1>
+    <div>
+      {showLogin ? (
+        <>
+          <LoginForm loginUser={loginUser} addErrors={addErrors} clearErrors={clearErrors} />
+          <br />
+          <p>
+            Don't have an account? &nbsp;
+            <button onClick={()=>setShowLogin(false)}>Sign up</button>
+          </p>
+        </>
+      ) : (
+        <>
+          <SignupForm loginUser={loginUser} addErrors={addErrors} clearErrors={clearErrors} />
+          <br />
+          <p>
+            Already have an account? &nbsp;
+            <button onClick={()=>setShowLogin(true)}>Sign in</button>
+          </p>
+        </>
+      )}
+    </div>
   )
 }
 
