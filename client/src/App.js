e3d832987e8208ee098a6b8b777ef1d38b1c0545
Marlena Tsang <Marlena345@gmail.com>
2022-03-19 23:19:39 -0400

    added error handling and login/out functions

--- client/src/App.js
+++ client/src/App.js
@@ -5,29 +5,81 @@ import NavBar from "./components/navigation/NavBar";
 import Login from "./components/pages/Login";
 import AddReview from "./components/pages/AddReview";
 import Home from "./components/pages/Home";
+import Errors from './components/static/Errors'
+import SignupForm from "./components/SignupForm";
+import {headers, getToken} from "./Global"
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [currentUser, setCurrentUser] = useState({});
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [errors, setErrors] = useState([])
 
-  useEffect(() => {
-    fetch("/me").then((r) => {
-      if (r.ok) {
-        r.json().then((user) => setUser(user));
-      }
-    });
-  }, []);
+  function loginUser(user){
+    setCurrentUser(user)
+    setLoggedIn(true)
+  }
 
-  if(!user) return <Login />
+  function logoutUser(){
+    setCurrentUser({});
+    setLoggedIn(false);
+    localStorage.removeItem('jwt')
+  }
+
+  useEffect(()=>{
+    const token = localStorage.getItem('jwt');
+    if(token && !loggedIn) {
+      fetch('/me',{
+        method: "GET",
+        headers:{
+          ...headers, ...getToken()
+        },
+      })
+      .then((r)=>r.json())
+      .then((user)=>loginUser(user))
+    }
+  },[loggedIn])
+
+  function addErrors(){
+    setErrors(errors)
+  }
+
+  function clearErrors(){
+    setErrors([])
+  }
 
   return (
     <div>
-      <NavBar user={user} setUser={setUser} />
+      <NavBar 
+        logoutUser={logoutUser}
+        loggedIn = {loggedIn}
+      />
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/addReview" element={<AddReview user={user} />} />
+          <Route 
+            path="/" 
+            element={<Home />} 
+          />
+          <Route 
+            path="/signup" 
+            element={<SignupForm 
+              loginUser={loginUser}
+              addErrors={addErrors}
+              clearErrors={clearErrors}/>} 
+          />
+          <Route 
+            path="/login"  
+            element={<Login 
+              loginUser={loginUser}
+              addErrors={addErrors} clearErrors={clearErrors} 
+            />} 
+          />
+          <Route 
+            path="/reviews/new" 
+            element={<AddReview currentUser={currentUser} />} 
+          />
         </Routes>
       </main>
+      <Errors errors={errors}/>
     </div>
   );
 }
