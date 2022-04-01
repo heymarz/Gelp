b9039f1b890e944841e0e968dacd2c44d0d9d468
Marlena Tsang <Marlena345@gmail.com>
2022-04-01 13:01:49 -0400

    edited format

--- client/src/components/RestaurantListing.jsx
+++ client/src/components/RestaurantListing.jsx
@@ -14,13 +14,13 @@ function RestaurantListing() {
     <div>
       <h4>Restaurant Listing</h4>
       {restaurants.map((r)=>{
-    return <div className="card" key={r.id}>
-        <h4>{r.name}</h4>
-        <p><em><b>Food Type:</b>{r.food_type}</em></p>
-        <p><b>Restaurant Description:</b><br />{r.description}</p>
-      </div>
-      })
-    }
+        return <div className="card" key={r.id}>
+                  <h4>{r.name}</h4>
+                  <p><em><b>Food Type:</b>{r.food_type}</em></p>
+                  <p><b>Restaurant Description:</b><br />{r.description}</p>
+                </div>
+        })
+      }
     </div>
   )
 }
