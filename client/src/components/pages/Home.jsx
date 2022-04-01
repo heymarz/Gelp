845ff33e4ee1a327382d4e6dcfbba16ee4b830c8
Marlena Tsang <Marlena345@gmail.com>
2022-04-01 12:55:03 -0400

    toggling login and logout restaurant display

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import RestaurantListing from '../RestaurantListing'
 
-function Home(){
+function Home({loggedIn}){
   return (
     <div>
       <h1>Welcome to Gelp!</h1>
       <h3>This is a website that you can count on. Rate restaurants and their services. Share your thoughts and opinions with Restuarant reviews! Can't wait to hear your thoughts! Happy Gelping!</h3>
-      <RestaurantListing />
+      {loggedIn? <RestaurantListing /> : <p>Please sign in at the link above.</p>}
     </div>
   )
 }
