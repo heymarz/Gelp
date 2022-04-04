f40bc6cf0ea9b01db7dd83ae2f3893df9a86b0b3
Marlena Tsang <Marlena345@gmail.com>
2022-04-04 18:42:18 -0400

    labels and inputs in form

--- client/src/components/pages/AddReview.jsx
+++ client/src/components/pages/AddReview.jsx
@@ -1,8 +1,84 @@
-import React from 'react'
+import React, {useState} from 'react';
+import './AddReview.css';
 
-function AddReview() {
+function AddReview({currentUser, restuarants}) {
+  const [description, setDescription] = useState("");
+  
   return (
-    <div>AddReview</div>
+    <div>
+      <h1>Add a new review for ...</h1>
+      <form>
+        <label htmlFor='description' id="textbox">Write your review in the box below:</label>
+        <div>
+        <textarea
+          type="text"
+          id="description"
+          value={description}
+          onChange={(e)=>setDescription(e.target.value)}
+        />
+        </div>
+        <button>Submit</button>
+        <label html="user"></label>
+        <input 
+          type="hidden" 
+          id="currentUser"
+          value={currentUser.id}
+        />
+        {/* <input
+          type="hidden"
+          id={restaurant.id}
+          value={restaurant.id}
+        /> */}
+        <br />
+        <fieldset className="star-rating">
+          <legend className="star-rating__title">Your rating:</legend>
+          <div className='star-rating__stars'>
+            <input
+              className="star-rating__label" 
+              type="radio" 
+              id="rating1" 
+              name="rating" 
+              value="1"
+            />
+            <label className="star-rating__label" htmlFor="rating1" aria-label="One"></label>
+            <input
+              className="star-rating__label" 
+              type="radio" 
+              id="rating2" 
+              name="rating" 
+              value="2"
+            />
+            <label className="star-rating__label" htmlFor="rating2" aria-label="Two"></label>
+            <input
+              className="star-rating__label"
+              type="radio" 
+              id="rating3" 
+              name="rating" 
+              value="3"
+            />
+            <label className="star-rating__label" htmlFor="rating3" aria-label="Three"></label>
+            <input
+              className="star-rating__label" 
+              type="radio" 
+              id="rating4" 
+              name="rating" 
+              value="4"
+            />
+            <label className="star-rating__label" htmlFor="rating4" aria-label="Four"></label>
+            <input
+              className="star-rating__label" 
+              type="radio" 
+              id="rating5" 
+              name="rating" 
+              value="5"
+            />
+            <label className="star-rating__label" htmlFor="rating5" aria-label='Five'></label>
+          </div>
+        </fieldset>
+      </form>
+
+    {/* submit at the back end: user.id, restuarnat.id,rating */}
+    </div>
   )
 }
 
