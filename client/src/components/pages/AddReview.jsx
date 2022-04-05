5538cac42dc80bf803974cc1cdf88d616b6dce4e
Marlena Tsang <Marlena345@gmail.com>
2022-04-05 11:01:45 -0400

    Added stars for rating

--- client/src/components/pages/AddReview.jsx
+++ client/src/components/pages/AddReview.jsx
@@ -1,83 +1,64 @@
 import React, {useState} from 'react';
 import './AddReview.css';
+import { FaStar } from "react-icons/fa";
 
 function AddReview({currentUser, restuarants}) {
   const [description, setDescription] = useState("");
+  const [rating, setRating] =useState(null);
+  const [hover, setHover] = useState(null)
   
   return (
-    <div>
+    <div className="reviewBody">
       <h1>Add a new review for ...</h1>
       <form>
-        <label htmlFor='description' id="textbox">Write your review in the box below:</label>
+        <h3><label htmlFor='description' id="textbox">Write your review in the box below:</label></h3>
         <div>
-        <textarea
-          type="text"
-          id="description"
-          value={description}
-          onChange={(e)=>setDescription(e.target.value)}
-        />
-        </div>
-        <button>Submit</button>
-        <label html="user"></label>
-        <input 
-          type="hidden" 
-          id="currentUser"
-          value={currentUser.id}
-        />
-        {/* <input
-          type="hidden"
-          id={restaurant.id}
-          value={restaurant.id}
-        /> */}
-        <br />
-        <fieldset className="star-rating">
           <legend className="star-rating__title">Your rating:</legend>
-          <div className='star-rating__stars'>
-            <input
-              className="star-rating__label" 
-              type="radio" 
-              id="rating1" 
-              name="rating" 
-              value="1"
-            />
-            <label className="star-rating__label" htmlFor="rating1" aria-label="One"></label>
-            <input
-              className="star-rating__label" 
-              type="radio" 
-              id="rating2" 
-              name="rating" 
-              value="2"
-            />
-            <label className="star-rating__label" htmlFor="rating2" aria-label="Two"></label>
-            <input
-              className="star-rating__label"
-              type="radio" 
-              id="rating3" 
-              name="rating" 
-              value="3"
-            />
-            <label className="star-rating__label" htmlFor="rating3" aria-label="Three"></label>
-            <input
-              className="star-rating__label" 
-              type="radio" 
-              id="rating4" 
-              name="rating" 
-              value="4"
-            />
-            <label className="star-rating__label" htmlFor="rating4" aria-label="Four"></label>
-            <input
-              className="star-rating__label" 
-              type="radio" 
-              id="rating5" 
-              name="rating" 
-              value="5"
-            />
-            <label className="star-rating__label" htmlFor="rating5" aria-label='Five'></label>
+          {[...Array(5)].map((star, i)=>{
+            const ratingValue = i + 1
+            return (
+            <label>
+              <input 
+                type="radio"
+                name="rating"
+                value={ratingValue}
+                onClick={()=>setRating(ratingValue)}
+                />
+              <FaStar 
+                className="star" 
+                size={30} 
+                color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
+                onMouseEnter={()=> setHover(ratingValue)}
+                onMouseLeave={()=>setHover(null)}
+              />
+            </label>)
+          })}
+          <br/>
+          <textarea
+            type="text"
+            id="description"
+            value={description}
+            onChange={(e)=>setDescription(e.target.value)}
+          />
           </div>
-        </fieldset>
+          <button>Submit</button>
+          <label html="user"></label>
+          <input 
+            type="hidden" 
+            id="currentUser"
+            value={currentUser.id}
+          />
+          {/* <input
+            type="hidden"
+            id={restaurant.id}
+            value={restaurant.id}
+          /> 
+          <input
+            type="hidden"
+            id={user.id}
+            value={user.id}
+          /> */}          
       </form>
-
-    {/* submit at the back end: user.id, restuarnat.id,rating */}
     </div>
   )
 }
