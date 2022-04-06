72fc453414ae016ab067b1b27dadf46909901798
Marlena Tsang <Marlena345@gmail.com>
2022-04-05 21:13:39 -0400

    added strict cookies config

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module ReactRailsApiProjectTemplate
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
 
+    config.action_dispatch_cookies_same_site_protection = :strict
     # Use SameSite=Strict for all cookies to help protect against CSRF
     # https://owasp.org/www-community/SameSite
     config.action_dispatch.cookies_same_site_protection = :strict
